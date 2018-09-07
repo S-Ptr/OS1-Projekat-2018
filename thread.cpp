70bb83310e23c507f0d333e147a0dc648a32dd7a
Stefan Petrović <hihitmen@hotmail.com>
2018-09-07 18:32:24 +0200

    Added 1(one) line in the Thread constructor

--- thread.cpp
+++ thread.cpp
@@ -1,17 +1,17 @@
 #include "thread.h"
 #include "PCB.h"
 
-extern volatile int zahtevana_promena_konteksta;
+extern volatile int csreq;
 
 void dispatch(){ // sinhrona promena konteksta 
 	asm cli;
-	zahtevana_promena_konteksta = 1;
+	csreq = 1;
 	timer();
 	asm sti;
 }
 
 Thread::Thread(StackSize stackSize = defaultStackSize, Time timeSlice = defaultTimeSlice){
-
+	myPCB = new PCB(this);
 }
 
 void Thread::waitToComplete(){
