6084aed9d04ddc65eadceaac1e285dc3d958f157
Stefan Petrović <hihitmen@hotmail.com>
2018-09-08 00:57:43 +0200

    Added idle thread support and outline of the main function

--- kernel.cpp
+++ kernel.cpp
@@ -1,20 +1,22 @@
 #include "kernel.h"
 #include "SCHEDULE.H"
 #include "PCB.h"
+#include "IdleThr.h"
 
 volatile PCB* running;
+volatile Thread* placeholder = new IdleThr();
 
 //temporary storage
 unsigned tsp;
 unsigned tss;
 unsigned tbp;
 
-volatile int brojac = 20;
-volatile int zahtevana_promena_konteksta = 0;
+volatile int counter = 20;
+volatile int csreq = 0;
 
 void interrupt timer(){	// prekidna rutina
-	if (!zahtevana_promena_konteksta) brojac--; 
-	if (brojac == 0 || zahtevana_promena_konteksta) {
+	if (!cseq) brojac--; 
+	if (brojac == 0 || csreq) {
 		asm {
 			// cuva sp
 			mov tsp, sp
@@ -29,7 +31,11 @@ void interrupt timer(){	// prekidna rutina
 		running->flag = 0x1;
 
 		running= Scheduler::get();	// Scheduler
-  
+  		
+		if(running == 0){
+			running = placeholder->myPCB;
+		}
+
 		tsp = running->sp;
 		tss = running->ss;
 		tbp = running->bp;
@@ -50,7 +56,73 @@ void interrupt timer(){	// prekidna rutina
      // poziva se samo kada nije zahtevana promena
      // konteksta – tako se da se stara
      // rutina poziva samo kada je stvarno doslo do prekida	
-	if(!zahtevana_promena_konteksta) asm int 60h;
+	if(!csreq) asm int 60h;
 		                                              
-	zahtevana_promena_konteksta = 0;
+	csreq = 0;
+}
+
+unsigned oldTimerOFF, oldTimerSEG; // stara prekidna rutina
+
+// postavlja novu prekidnu rutinu
+void setup(){
+	asm{
+		cli
+		push es
+		push ax
+
+		mov ax,0   //  ; inicijalizuje rutinu za tajmer
+		mov es,ax
+
+		mov ax, word ptr es:0022h //; pamti staru rutinu
+		mov word ptr oldTimerSEG, ax	
+		mov ax, word ptr es:0020h	
+		mov word ptr oldTimerOFF, ax	
+
+		mov word ptr es:0022h, seg timer	 //postavlja 
+		mov word ptr es:0020h, offset timer //novu rutinu
+
+		mov ax, oldTimerSEG	 //	postavlja staru rutinu	
+		mov word ptr es:0182h, ax //; na int 60h
+		mov ax, oldTimerOFF
+		mov word ptr es:0180h, ax
+
+		pop ax
+		pop es
+		sti
+	}
+}
+
+void restore(){
+	asm {
+		cli
+		push es
+		push ax
+
+		mov ax,0
+		mov es,ax
+
+
+		mov ax, word ptr oldTimerSEG
+		mov word ptr es:0022h, ax
+		mov ax, word ptr oldTimerOFF
+		mov word ptr es:0020h, ax
+
+		pop ax
+		pop es
+		sti
+	}
+}
+
+extern int userMain(int argc, char* argv[]);
+int returnval;
+
+int main (int argc, char* argv[]);{
+	setup();
+
+	//??
+	returnval = userMain(argc, argv);
+
+	restore();
+
+	return returnval;
 }
